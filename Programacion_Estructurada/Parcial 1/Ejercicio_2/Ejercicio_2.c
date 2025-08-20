e28badea168030634d2a7fff3ffafac748790da3
Mariobpb <mariobpb27@gmail.com>
2025-08-19 20:50:05 -0600

    a

--- Programacion_Estructurada/Parcial 1/Ejercicio_2/Ejercicio_2.c
+++ Programacion_Estructurada/Parcial 1/Ejercicio_2/Ejercicio_2.c
@@ -1,1 +1,19 @@
-//Hola
+/*
+Ejercicio 2: Cálculo de área y perímetro
+
+Escribe un programa que pregunte al usuario los dos lados de un rectángulo y presente por pantalla el
+cálculo del perímetro (suma de los lados) y el área (base por altura).
+*/
+
+#include <stdio.h>
+
+int main() {
+    int width = 0;
+    int height = 0;
+
+    do {
+
+    }while ();
+
+    return 0;
+}
