0d9d650ab2921485a559a06b1c899eb07a5c4185
Mariobpb <mariobpb27@gmail.com>
2025-08-28 12:35:10 -0600

    Ejercicio_22_08_2025 Completado

--- Programacion_Estructurada/Parcial_1/Ejercicio_22_08_2025/Ejercicio_22_08_2025.c
+++ Programacion_Estructurada/Parcial_1/Ejercicio_22_08_2025/Ejercicio_22_08_2025.c
@@ -2,8 +2,33 @@
 */
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
-    printf("Hello world");
+    int calificaciones[8];
+    for (int materia = 0; materia < 8; materia++)
+    {
+        printf("Ingrese la calificación de la materia #%d:\n", materia+1);
+        int intentos = 3;
+        do{
+            scanf("%d", &calificaciones[materia]);
+            if(calificaciones[materia] < 0 || calificaciones[materia] > 100) {
+                printf("Calificación inválida\n");
+                intentos--;
+                if (intentos == 0)
+                {
+                    printf("Intentos agotados, definiendo calificación como 0...\n");
+                    calificaciones[materia] = 0;
+                    break;
+                }
+            } else break;
+        } while(intentos > 0);
+    }
+    printf("\nCalificaciones ingresadas:\n");
+    for (int m = 0; m < 8; m++)
+    {
+        printf("\nMateria #%d: %d", m+1, calificaciones[m]);
+    }
+    
     return 0;
 }
