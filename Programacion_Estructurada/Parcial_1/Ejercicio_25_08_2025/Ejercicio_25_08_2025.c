2cf5d8708611b1fffcc2bec33d0ca69f111f6e26
Mariobpb <mariobpb27@gmail.com>
2025-08-28 12:11:27 -0600

    Ejercicio_25_08_2025 Completado

--- Programacion_Estructurada/Parcial_1/Ejercicio_25_08_2025/Ejercicio_25_08_2025.c
+++ Programacion_Estructurada/Parcial_1/Ejercicio_25_08_2025/Ejercicio_25_08_2025.c
@@ -9,24 +9,28 @@
 int main() {
     char materias[8][25], tempM[25];
     for(int m = 0; m < 8; m++){
-        printf("Ingrese el nombre de la materia #%d:\n");
+        printf("Ingrese el nombre de la materia #%d:\n", m+1);
         bool stringVerified = false;
         do{
             fgets(tempM, 25, stdin);
             int n = 0;
-            while(tempM[n] != '\0'){
-                if(tempM[n] > 'a' && tempM[n] < 'z')
+            while(tempM[n] != '\0' || tempM[n] != '\n'){
+                if((tempM[n] >= 'a' && tempM[n] <= 'z') || (tempM[n] >= 'A' && tempM[n] <= 'Z'))
                 {
                     n++;
-                    if(tempM[n] == '\0')
+                    if(tempM[n] == '\0' || tempM[n] == '\n')
                     {
                         stringVerified = true;
+                        break;
                     }
                 }
-                else break;
+                else {
+                    printf("Caracteres inválidos\n");
+                    break;
+                }
             }
             if (stringVerified) strcpy(materias[m], tempM);
-        }while(!stringVerified)
+        }while(!stringVerified);
     }
     return 0;
 }
