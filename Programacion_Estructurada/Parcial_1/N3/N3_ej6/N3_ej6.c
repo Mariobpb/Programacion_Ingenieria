f91724d1dc4c1ff2504fb83936b8ea36cc2b2575
Mariobpb <mariobpb27@gmail.com>
2025-09-07 23:52:14 -0600

    N3 ej 6 y 7 hecho

--- Programacion_Estructurada/Parcial_1/N3/N3_ej6/N3_ej6.c
+++ Programacion_Estructurada/Parcial_1/N3/N3_ej6/N3_ej6.c
@@ -1,13 +1,91 @@
 /*
-Ejercicio 6: 
-
+Ejercicio 6: Media, mayor y menor de un conjunto prefijado. 
+Igual que el apartado anterior pero en vez de 20 alumnos ahora el número de alumnos se 
+le preguntará al usuario al iniciar el programa, este número no podrá superar los 100 
+alumnos (controlar que el usuario introduzca un número menor que 100). 
 */
 
 #include <stdio.h>
 
+int cantAlumnos;
+
+void listarNotas(int *cals);
+float calcularMedia(int *cals);
+int calcularMenor(int *cals);
+int calcularMayor(int *cals);
+
 int main()
 {
-    
-    
+    int aux, calificaciones[100], opcion;
+    do
+    {
+        printf("Ingrese la cantidad del alumnos : ", &aux);
+        scanf("%d", &aux);
+    } while (aux < 1 || aux > 100);
+    cantAlumnos = aux;
+    for (int i = 0; i < cantAlumnos; i++)
+    {
+        printf("Ingrese la calificacion del alumno #%d: ", i + 1);
+        scanf("%d", &calificaciones[i]);
+    }
+    do
+    {
+        printf("\n\nQué desea hacer?\n1. Listar notas, 2.Calcular la media, 3.Calcular el menor, 4.Calcular el mayor, 5.Salir:  ");
+        scanf("%d", &opcion);
+        switch (opcion)
+        {
+        case 1:
+            listarNotas(calificaciones);
+            break;
+        case 2:
+            printf("\nMedia de calificaciones: %.2f\n", calcularMedia(calificaciones));
+            break;
+        case 3:
+            printf("\nMenor calificacion: %d\n", calcularMenor(calificaciones));
+            break;
+        case 4:
+            printf("\nMayor calificacion: %d\n", calcularMayor(calificaciones));
+            break;
+        default:
+            break;
+        }
+    } while (opcion != 5);
+
     return 0;
+}
+
+void listarNotas(int *cals)
+{
+    printf("\nNotas:\n");
+    for (int i = 0; i < cantAlumnos; i++)
+    {
+        printf("\nAlumno #%d: %d", i + 1, cals[i]);
+    }
+}
+float calcularMedia(int *cals)
+{
+    float res = 0;
+    for (int i = 0; i < cantAlumnos; i++)
+        res += cals[i];
+    return res / (float)cantAlumnos;
+}
+int calcularMenor(int *cals)
+{
+    int numMenor = cals[0];
+    for (int i = 0; i < cantAlumnos; i++)
+    {
+        if (cals[i] < numMenor)
+            numMenor = cals[i];
+    }
+    return numMenor;
+}
+int calcularMayor(int *cals)
+{
+    int numMayor = cals[0];
+    for (int i = 0; i < cantAlumnos; i++)
+    {
+        if (cals[i] > numMayor)
+            numMayor = cals[i];
+    }
+    return numMayor;
 }
