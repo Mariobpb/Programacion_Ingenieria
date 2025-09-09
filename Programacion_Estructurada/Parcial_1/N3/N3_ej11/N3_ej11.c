43aa2a22b1167b91b715e1f91907b2c6d2d63023
Mariobpb <mariobpb27@gmail.com>
2025-09-09 14:26:15 -0600

    N3 ej 11 y 12 hechos

--- Programacion_Estructurada/Parcial_1/N3/N3_ej11/N3_ej11.c
+++ Programacion_Estructurada/Parcial_1/N3/N3_ej11/N3_ej11.c
@@ -3,28 +3,30 @@ Ejercicio 11: Arrays multidimensionales. Edificio3.
 Se quiere controlar el número de habitantes de un eidificio con 6 pisos y 4 puertas (A,
 B, C, y D) en cada piso.
 Realizar un programa que pida al usuario que introduzca el número de habitantes de
-cada puerta del edificio. El programa debe mostrar la media de habitantes de cada piso. 
+cada puerta del edificio. El programa debe mostrar la media de habitantes de cada piso.
 */
 
 #include <stdio.h>
 
 int main()
 {
-    int edificio[6][4], sumaHabitantes = 0, mediaHabitantes;
+    int edificio[6][4];
+    float mediaHabitantesPiso[6];
     for (int i = 0; i < 6; i++)
     {
+        int sumaHabitantes = 0;
         for (int j = 0; j < 4; j++)
         {
             printf("Ingrese el numero de habitantes del piso #%d, puerta %c: ", i + 1, 65 + j);
             scanf("%d", &edificio[i][j]);
-            if (edificio[i][j] > habitantesMax){
-                habitantesMax = edificio[i][j];
-                pisoMax = i;
-                puertaMax = j;
-            }
+            sumaHabitantes += edificio[i][j];
         }
+        mediaHabitantesPiso[i] = ((float)sumaHabitantes / 4);
     }
-    printf("\nLa vivienda con mas habitantes se encuentra en el piso #%d, puerta %c, con %d habitantes", pisoMax + 1, 65 + puertaMax, habitantesMax);
-    
+    for (int i = 0; i < 6; i++)
+    {
+        printf("\nLa media de habitantes del piso #%d es de %.2f habitantes", i + 1, mediaHabitantesPiso[i]);
+    }
+
     return 0;
 }
--- Programacion_Estructurada/Parcial_1/N3/N3_ej12/N3_ej12.c
+++ Programacion_Estructurada/Parcial_1/N3/N3_ej12/N3_ej12.c
@@ -1,13 +1,58 @@
 /*
-Ejercicio 12: 
-
+Ejercicio 12: Arrays multidimensionales. Rectas en el plano.
+Se quiere representar rectas en un plano de números enteros. El plano será un array de
+100x100 caracteres. El programa pedirá que se introduzcan los tres coeficientes
+(números enteros) de una recta de la forma ax+by+c=0, representará los puntos de la
+recta como asteriscos en la matriz, y presentará por pantalla dicha matriz.
 */
 
 #include <stdio.h>
 
+int resolverX(float a, float b, float c, float y);
+
 int main()
 {
-    
-    
+    char plano[100][100];
+    float a, b, c;
+
+    printf("Introduce los siguientes valores:\n");
+    printf("a: ");
+    scanf("%f", &a);
+    printf("b: ");
+    scanf("%f", &b);
+    printf("c: ");
+    scanf("%f", &c);
+
+    for (int y = 99; y >= 0; y--)
+    {
+        int xR = resolverX(a, b, c, y-50);
+        printf("x%d=%d\n", y, xR);
+        for (int x = 0; x < 100; x++)
+        {
+            if (y != 50)
+                plano[x][y] = ' ';
+            else
+                plano[x][y] = '-';
+            if (x == 50)
+                plano[x][y] = '|';
+            if (x == xR+50)
+                plano[x][y] = '*';
+        }
+    }
+    printf("\n");
+    for (int y = 99; y >= 0; y--)
+    {
+        for (int x = 0; x < 100; x++)
+        {
+            printf("%c", plano[x][y]);
+        }
+        printf("\n");
+    }
+
     return 0;
+}
+
+int resolverX(float a, float b, float c, float y)
+{
+    return (-((b * y) + c)) / a;
 }
