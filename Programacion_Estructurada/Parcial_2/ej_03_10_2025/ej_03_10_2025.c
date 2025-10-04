892eeab27f5ac2541700432e78f70024a9ec6096
Mariobpb <mariobpb27@gmail.com>
2025-10-04 14:58:23 -0600

    Contador de palabras (por valor) hecho

--- Programacion_Estructurada/Parcial_2/ej_03_10_2025/ej_03_10_2025.c
+++ Programacion_Estructurada/Parcial_2/ej_03_10_2025/ej_03_10_2025.c
@@ -1,35 +1,59 @@
 #include <stdio.h>
 
-void leerCadena(char *cad);
-void mostrarCantPalabras(char *cad);
+void leerCadena(char *c);
+void reemplazarEDtoNull(char *c);
+void mostrarPalabras(char *c);
+int contarPalabras(char *c);
+int recorrerPalabra(char *c, int p);
+int recorrerEspacios(char *c, int p);
 
 int main()
 {
     char cad[50];
     leerCadena(cad);
-    mostrarCantPalabras(cad);
+    mostrarPalabras(cad);
     return 0;
 }
 
-void leerCadena(char *cad)
+void leerCadena(char *c)
 {
     printf("Ingrese una cadena:\n");
-    fgets(cad, 50, stdin);
+    fgets(c, 50, stdin);
+    reemplazarEDtoNull(c);
 }
-int contarPalabras(char *cad)
+void reemplazarEDtoNull(char *c)
 {
-    int i = 0, palabras = 0, palabraIniciada = 0;
-    while (cad[i] != '\n')
+    int i = 0;
+    while (c[i] != '\n')
+        i++;
+    c[i] = '\0';
+}
+void mostrarPalabras(char *c)
+{
+    printf("El texto tiene %d palabras", contarPalabras(c));
+}
+int contarPalabras(char *c)
+{
+    int numPal = 0, i = 0;
+    while (c[i] != '\0')
     {
-        if ((cad[i] >= 'a' && cad[i] <= 'z') || (cad[i] >= 'A' && cad[i] <= 'Z'))
-            palabraIniciada = 1;
-        if (cad[i+1] == ' ' || cad[i+1] == '\0'){
-            pa
-        }
+        i = recorrerEspacios(c, i);
+        int aux = i;
+        i = recorrerPalabra(c, i);
+        if (i != aux)
+            numPal++;
     }
-    return palabras;
+    return numPal;
 }
-void mostrarCantPalabras(char *cad)
+int recorrerEspacios(char *c, int p)
 {
-    printf("%s", cad);
+    while (c[p] != '\0' && c[p] == ' ')
+        p++;
+    return p;
+}
+int recorrerPalabra(char *c, int p)
+{
+    while (c[p] != ' ' && c[p] != '\0')
+        p++;
+    return p;
 }
