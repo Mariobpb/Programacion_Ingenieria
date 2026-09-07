475b2efbde0d7aaf7d69f900edc0d22bf5e05971
Mariobpb <mariobpb27@gmail.com>
2026-09-07 03:39:13 -0600

    lel

---
+++ POE/Parcial_1/Practica2/src/vista/VentanaFecha.java
@@ -0,0 +1,28 @@
+package vista;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class VentanaFecha {
+    private JFrame vtn = null;
+    private JTextField txtDia = null;
+    private JTextField txtMes = null;
+    private JTextField txtAnio = null;
+    private JButton btnVerificar = null;
+    private JLabel lblRes = null;
+
+    public VentanaFecha() {
+        vtn = new JFrame();
+        txtDia = new JTextField();
+        txtMes = new JTextField();
+        txtAnio = new JTextField();
+        btnVerificar = new JButton();
+        lblRes = new JLabel();
+    }
+
+    public void dibujarVentana() {
+
+    }
+}
