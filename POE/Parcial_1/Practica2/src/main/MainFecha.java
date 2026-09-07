4acd4eb4d8da101c475608c4e818647d503937cb
Mariobpb <mariobpb27@gmail.com>
2026-09-07 03:44:55 -0600

    xqaf

--- POE/Parcial_1/Practica2/src/main/MainFecha.java
+++ POE/Parcial_1/Practica2/src/main/MainFecha.java
@@ -1,5 +1,11 @@
 package main;
 
 public class MainFecha {
-    
+    public static void main(String[] args) {
+        Fecha objFecha = new Fecha();
+        VentanaFecha vtnFecha = new VentanaFecha();
+        ControladorFecha ctrlFecha = new ControladorFecha(objFecha, vtnFecha);
+
+        vtnFecha.dibujarVentana();
+    }
 }
